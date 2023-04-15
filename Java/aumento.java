eb306c17d0a68a0f255b5375df3b792cc383dd9d
Renata Rodrigues <ticare1@hotmail.com>
2023-04-15 16:11:06 -0300

    Exercicios em Java - Aumento

---
+++ Java/aumento.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aumento {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+	
+		double salario, novoSalario, aumento;
+		int porcentagem;
+		
+		System.out.print("Digite o salario da pessoa: ");
+		salario = sc.nextDouble();
+		
+		if (salario <= 1000.0) {
+			porcentagem = 20;;
+		} else if (salario <= 3000.0) {
+			porcentagem = 15;			
+		} else if (salario <= 8000.0) {
+			porcentagem = 10;			
+		} else {
+			porcentagem = 10;
+		}
+		
+		aumento = (salario * porcentagem) / 100;
+		novoSalario = salario + aumento;
+		
+		System.out.printf("Novo salario = R$ %.2f\n", novoSalario);
+		System.out.printf("Aumento = R$ %.2f\n", aumento);
+		System.out.printf("Porcentagem = %d %%\n", porcentagem);
+		
+		sc.close();
+	}
+}
