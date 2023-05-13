0c8a3de638fcf927aa81cc0dbc3d22f11fbe3491
Renata Rodrigues <ticare1@hotmail.com>
2023-05-13 18:27:40 -0300

    Exercicios em C# - Circulo

---
+++ C#/circulo/circulo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace circulo {
+    class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double raio, area;
+
+            Console.Write("Digite o valor do raio do circulo: ");
+            raio = double.Parse(Console.ReadLine(), CI);
+
+            area = Math.PI * (Math.Pow(raio, 2));
+            Console.WriteLine("AREA = " + area.ToString("F3", CI));
+        }
+    }
+}
