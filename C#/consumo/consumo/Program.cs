2deaa95161d95fbe7920953e380344aa3f55e078
Renata Rodrigues <ticare1@hotmail.com>
2023-05-13 19:17:33 -0300

    Exercicios em C# - Consumo

---
+++ C#/consumo/consumo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace consumo {
+    class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int distancia;
+            double combustivel, consumoMedio;
+
+            Console.Write("Distancia percorrida: ");
+            distancia = int.Parse(Console.ReadLine());
+            Console.Write("Combustível gasto: ");
+            combustivel = double.Parse(Console.ReadLine(), CI);
+
+            consumoMedio = distancia / combustivel;
+            Console.WriteLine("Consumo medio = " + consumoMedio.ToString("F3", CI));
+        }
+    }
+}
