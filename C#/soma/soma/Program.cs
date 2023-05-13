ba978ee95e4506865c9c1cdc525943022c445739
Renata Rodrigues <ticare1@hotmail.com>
2023-05-13 18:08:24 -0300

    Exercicios em C# - Soma

---
+++ C#/soma/soma/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace soma {
+    class Program {
+        static void Main(string[] args) {
+            int X, Y, soma;
+
+            Console.Write("Digite o valor de X: ");
+            X = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y: ");
+            Y = int.Parse(Console.ReadLine());
+
+            soma = X + Y;
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
