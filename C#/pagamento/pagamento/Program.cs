11367bc6792802cc4f328e2e90a330cf5338d88f
Renata Rodrigues <ticare1@hotmail.com>
2023-05-13 19:05:41 -0300

    Exercicios em C# - Pagamento

---
+++ C#/pagamento/pagamento/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace pagamento {
+    class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            string nome;
+            double vlHora, pagamento;
+            int qtdHorasTrab;
+
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Valor por hora: ");
+            vlHora = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Horas Trabalhadas: ");
+            qtdHorasTrab = int.Parse(Console.ReadLine());
+
+            pagamento = vlHora * qtdHorasTrab;
+            Console.WriteLine("O pagamento para " + nome + " deve ser " + pagamento.ToString("F2", CI));
+        }
+    }
+}
