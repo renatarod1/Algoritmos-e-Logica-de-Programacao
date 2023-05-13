42b89b69e44561f9ab783293330fcf5e3b23e80a
Renata Rodrigues <ticare1@hotmail.com>
2023-05-13 18:18:09 -0300

    Exercicios em C# - Troco

---
+++ C#/troco/troco/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace troco {
+    class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double preco, troco, vlRecebido;
+            int qtd;
+
+            Console.Write("Preço unitário do produto: ");
+            preco = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Quantidade comprada: ");
+            qtd = int.Parse(Console.ReadLine());
+
+            Console.Write("Dinheiro recebido: ");
+            vlRecebido = double.Parse(Console.ReadLine(), CI);
+
+            troco = vlRecebido - (qtd * preco);
+            Console.WriteLine("TROCO = " + troco.ToString("F2", CI));
+        }
+    }
+}
