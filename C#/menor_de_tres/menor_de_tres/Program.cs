02f6794132ba455afc6c6a39a0a9bbc4b785ebdd
Renata Rodrigues <ticare1@hotmail.com>
2023-05-16 18:22:22 -0300

    Exercicios em C# - Menor_de_Tres

---
+++ C#/menor_de_tres/menor_de_tres/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace menor_de_tres {
+    internal class Program {
+        static void Main(string[] args) {
+            int primeiroVl, segundoVl, terceiroVl, menor;
+
+            Console.Write("Primeiro valor: ");
+            primeiroVl = int.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            segundoVl = int.Parse(Console.ReadLine());
+
+            Console.Write("Terceiro valor: ");
+            terceiroVl = int.Parse(Console.ReadLine());
+
+            if (primeiroVl < segundoVl && primeiroVl < terceiroVl) {
+                Console.WriteLine("MENOR = " + primeiroVl);
+            }
+            else if (segundoVl < terceiroVl) {
+                Console.WriteLine("MENOR = " + segundoVl);
+            }
+            else {
+                Console.WriteLine("MENOR = " + terceiroVl);
+            }
+        }
+    }
+}
