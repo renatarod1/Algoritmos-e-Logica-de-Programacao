14383c09a7c90e9aa370ad62ea9f10baa06e1f42
Renata Rodrigues <ticare1@hotmail.com>
2023-05-16 19:56:01 -0300

    Exercicios em C# - Lanchonete

---
+++ C#/lanchonete/lanchonete/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Linq.Expressions;
+
+namespace lanchonete {
+    internal class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int codProd, qtdProd;
+            double valorTotal;
+
+            Console.Write("Codigo do produto comprado: ");
+            codProd = int.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade do produto comprado: ");
+            qtdProd = int.Parse(Console.ReadLine());
+
+            valorTotal = 0.0;
+            switch (codProd) {
+                case 1:
+                    valorTotal = qtdProd * 5.0;
+                    break;
+
+                case 2:
+                    valorTotal = qtdProd * 3.5;
+                    break;
+
+                case 3:
+                    valorTotal = qtdProd * 4.8;
+                    break;
+
+                case 4:
+                    valorTotal = qtdProd * 8.9;
+                    break;
+
+                case 5:
+                    valorTotal = qtdProd * 7.32;
+                    break;
+            }
+
+            Console.WriteLine("Valor a pagar: R$ " + valorTotal.ToString("F2", CI));
+        }
+    }
+}
