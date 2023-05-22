ddd96505db3b9bc4f5bd4f8d820e67a2a68620eb
Renata Rodrigues <ticare1@hotmail.com>
2023-05-22 19:07:13 -0300

    Exercicios em C# - Aumento

---
+++ C#/aumento/aumento/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace aumento {
+    internal class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double salario, novoSalario, aumento;
+            int porcentagem;
+
+            Console.Write("Digite o salario da pessoa: ");
+            salario = double.Parse(Console.ReadLine(), CI);
+
+            if (salario <= 1000.0) {
+                porcentagem = 20;
+            }
+            else if (salario <= 3000.0) {
+                porcentagem = 15;
+            }
+            else if (salario <= 8000.0) {
+                porcentagem = 10;
+            }
+            else {
+                porcentagem = 5;
+            }
+
+            aumento = salario * porcentagem / 100;
+            novoSalario = salario + aumento;
+
+            Console.WriteLine("Novo salario = R$ " + novoSalario.ToString("F2", CI));
+            Console.WriteLine("Aumento = R$ " + aumento.ToString("F2", CI));
+            Console.WriteLine("Porcentagem = " + porcentagem  + " %");
+        }
+    }
+}
