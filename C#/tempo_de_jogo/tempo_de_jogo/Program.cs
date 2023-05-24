f9b04d45da4ac51b5029293ccee66bc88e852d0d
Renata Rodrigues <ticare1@hotmail.com>
2023-05-24 18:23:58 -0300

    Exercicios em C# - Tempo de Jogo

---
+++ C#/tempo_de_jogo/tempo_de_jogo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace tempo_de_jogo {
+    internal class Program {
+        static void Main(string[] args) {
+            int hi, hf, duracao;
+
+            Console.Write("Hora inicial: ");
+            hi = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Hora final: ");
+            hf = int.Parse(Console.ReadLine());
+
+            if (hi < hf) {
+                duracao = hf - hi;
+            }
+            else {
+
+                duracao = 24 - (hi - hf);
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
