be987fa538b1c92897ee06e63567388dd5c30517
Renata Rodrigues <ticare1@hotmail.com>
2023-05-24 20:12:03 -0300

    Exercicios em C# - Coordenadas

---
+++ C#/coordenadas/coordenadas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace coordenadas {
+    internal class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double x, y;
+            string localizacao;
+
+            Console.Write("Valor de X: ");
+            x = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Valor de Y: ");
+            y = double.Parse(Console.ReadLine(), CI);
+
+            if (x > 0 && y > 0) {
+                localizacao = "Q1";
+            }
+            else if (x > 0 && y < 0) {
+                localizacao = "Q4";
+            }
+            else if (x < 0 && y < 0) {
+                localizacao = "Q3";
+            }
+            else if (x < 0 && y > 0) {
+                localizacao = "Q2";
+            }
+            else if (x == 0 && y == 0) {
+                localizacao = "Origem";
+            }
+            else {
+                if (x == 0) {
+                    localizacao = "Eixo Y";
+                }
+                else {
+                    localizacao = "Eixo X";
+                }
+            }
+
+            Console.WriteLine(localizacao);
+        }
+    }
+}
