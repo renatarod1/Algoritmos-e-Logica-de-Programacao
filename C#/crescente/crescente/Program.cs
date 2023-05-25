69ded7975324ace2552a41aee11c0c86df42f7c0
Renata Rodrigues <ticare1@hotmail.com>
2023-05-25 18:19:04 -0300

    Exercicios em C# - Crescente

---
+++ C#/crescente/crescente/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace crescente {
+    internal class Program {
+        static void Main(string[] args) {
+            int x, y;
+
+            Console.WriteLine("Digite dois numeros:");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            while (x != y) {
+                if (x < y) {
+                    Console.WriteLine("CRESCENTE!");
+                }
+                else {
+                    Console.WriteLine("DECRESCENTE!");
+                }
+                Console.WriteLine("Digite outros dois numeros:");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
