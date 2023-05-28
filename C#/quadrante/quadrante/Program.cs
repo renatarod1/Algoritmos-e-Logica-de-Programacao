d4a275780a1b39b043c543c74676a0e907b9ce3f
Renata Rodrigues <ticare1@hotmail.com>
2023-05-28 11:24:16 -0300

    Exercicios em C# - Quadrante

---
+++ C#/quadrante/quadrante/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace quadrante {
+    internal class Program {
+        static void Main(string[] args) {
+            int x, y;
+
+            Console.WriteLine("Digite os valores das coordenadas X e Y:");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            while (x != 0 && y != 0) {
+                if (x > 0 && y > 0) {
+                    Console.WriteLine("Quadrante Q1");
+                }
+                else if (x > 0 && y < 0) {
+                    Console.WriteLine("Quadrante Q4");
+                }
+                else if (x < 0 && y < 0) {
+                    Console.WriteLine("Quadrante Q3");
+                }
+                else {
+                    Console.WriteLine("Quadrante Q2");
+                }
+
+                Console.WriteLine("Digite os valores das coordenadas X e Y:");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
