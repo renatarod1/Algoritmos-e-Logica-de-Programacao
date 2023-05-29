89c60adfadfe9acb82efe25d5affc88d173b94ad
Renata Rodrigues <ticare1@hotmail.com>
2023-05-28 21:09:08 -0300

    Exercicios em C# - Validacao de Nota

---
+++ C#/validacao_de_nota/validacao_de_nota/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace validacao_de_nota {
+    internal class Program {
+        static void Main(string[] args) {
+            double n1, n2, media;
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a primeira nota: ");
+            n1 = double.Parse(Console.ReadLine(), CI);
+
+            while (n1 < 0.0 || n1 > 10.0) {
+                Console.Write("Valor invalido! Tente novamente: ");
+                n1 = double.Parse(Console.ReadLine(), CI);
+            }
+
+            Console.Write("Digite a segunda nota: ");
+            n2 = double.Parse(Console.ReadLine(), CI);
+
+            while (n2 < 0.0 || n2 > 10.0) {
+                Console.Write("Valor invalido! Tente novamente: ");
+                n2 = double.Parse(Console.ReadLine(), CI);
+            }
+
+            media = (n1 + n2) / 2.0;
+            Console.WriteLine("MEDIA = " + media.ToString("F2", CI));
+        }
+    }
+}
