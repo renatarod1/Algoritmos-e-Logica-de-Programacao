81fe8a09f09e26398bde51ae9734807db893603f
Renata Rodrigues <ticare1@hotmail.com>
2023-06-01 18:48:04 -0300

    Exercicios em C# - Sequencia Impares

---
+++ C#/sequencia_impares/sequencia_impares/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace sequencia_impares {
+    internal class Program {
+        static void Main(string[] args) {
+            int x;
+
+            Console.Write("Digite o valor de X: ");
+            x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++) {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
