d96ed63ba3abe6b06e02ba5da39dc1afe7df1d57
Renata Rodrigues <ticare1@hotmail.com>
2023-06-01 18:14:08 -0300

    Exercicios em C# - Tabuada

---
+++ C#/tabuada/tabuada/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace tabuada {
+    internal class Program {
+        static void Main(string[] args) {
+            int n;
+
+            Console.Write("Deseja a tabuada para qual valor? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10;  i++) {
+                Console.WriteLine(n + " X "+ i + " = " + n * i);
+            }
+        }
+    }
+}
