fd7fc8e079532253ce591972a84d08eff57e4d2b
Renata Rodrigues <ticare1@hotmail.com>
2023-06-01 18:07:36 -0300

    Exercicios em C# - Pares Consecutivos - versao alternativa

---
+++ C#/pares_consecutivos/pares_consecutivos/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace pares_consecutivos {
+    internal class Program {
+        static void Main(string[] args) {
+            int x, somaPares;
+
+            Console.Write("Digite um numero inteiro: ");
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0) {
+                somaPares = 0;
+                if (x % 2 != 0) {
+                    x++;
+                }
+                for (int i = 0; i < 5; i++) {
+                    somaPares = somaPares + x;
+                    x = x + 2;
+                }
+                Console.WriteLine("SOMA = " + somaPares);
+
+                Console.Write("Digite um numero inteiro: ");
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
