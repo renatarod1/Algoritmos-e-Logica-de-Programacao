a8eccf68d28729de9b9a46a3122701ec9842c85d
Renata Rodrigues <ticare1@hotmail.com>
2023-06-02 20:06:48 -0300

    Exercicios em C# - Media Ponderada

---
+++ C#/media_ponderada/media_ponderada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace media_ponderada {
+    internal class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int n;
+            double n1, n2, n3, media;
+
+            Console.Write("Quantos casos voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Digite tres numeros:");
+                n1 = double.Parse(Console.ReadLine(), CI);
+                n2 = double.Parse(Console.ReadLine(), CI);
+                n3 = double.Parse(Console.ReadLine(), CI);
+
+                media = (2 * n1 + 3 * n2 + 5 * n3) / 10.0;
+                Console.WriteLine("MEDIA = " + media.ToString("F1", CI));
+            }
+        }
+    }
+}
