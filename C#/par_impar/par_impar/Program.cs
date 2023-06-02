0781a10569a973fd754f0d49d130a2d997374f93
Renata Rodrigues <ticare1@hotmail.com>
2023-06-02 19:50:36 -0300

    Exercicios em C# - Par Impar - versao alternativa

---
+++ C#/par_impar/par_impar/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace par_impar {
+    internal class Program {
+        static void Main(string[] args) {
+            int n, num;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                Console.Write("Digite um numero: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num == 0) {
+                    Console.WriteLine("NULO");
+                }
+                else {
+                    if (num % 2 == 0) {
+                        Console.Write("PAR ");
+                    }
+                    else {
+                        Console.Write("IMPAR ");
+                    }
+
+                    if (num < 0) {
+                        Console.WriteLine("NEGATIVO ");
+                    }
+                    else {
+                        Console.WriteLine("POSITIVO ");
+                    }
+                }
+            }
+        }
+    }
+}
