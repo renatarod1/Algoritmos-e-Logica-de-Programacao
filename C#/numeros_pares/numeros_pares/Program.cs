47fb65eeb4ec9475f45e586b279a177352803e29
Renata Rodrigues <ticare1@hotmail.com>
2023-06-03 10:55:50 -0300

    Exercicios em C# - Numeros Pares

---
+++ C#/numeros_pares/numeros_pares/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace numeros_pares {
+    internal class Program {
+        static void Main(string[] args) {
+            int n, contPares;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                Console.Write("Digite um numero: ");
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("NUMEROS PARES:");
+            contPares = 0;
+            for (int i = 0; i < n; i++) {
+                if (vet[i] % 2 == 0) {
+                    Console.Write(vet[i] + " ");
+                    contPares++;
+                }
+            }
+
+            Console.WriteLine("\n\nQUANTIDADE DE PARES = " + contPares);
+        }
+    }
+}
