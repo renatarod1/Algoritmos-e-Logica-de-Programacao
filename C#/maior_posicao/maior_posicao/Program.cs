7b85556c50284aa9b5d1e2d82ecee04789d51900
Renata Rodrigues <ticare1@hotmail.com>
2023-06-04 10:02:29 -0300

    Exercicios em C# -Maior Posicao

---
+++ C#/maior_posicao/maior_posicao/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace maior_posicao {
+    internal class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int n;
+            double maior, posicao;
+
+            Console.Write("Quanto numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (int i = 0; i < n; i++) {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            maior = 0;
+            posicao = 0;
+            for (int i = 0; i < n; i++) {
+                if (vet[i] > maior) {
+                    maior = vet[i];
+                    posicao = i;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("MAIOR VALOR = " + maior.ToString("F1", CI));
+            Console.WriteLine("POSICAO DO MAIOR VALOR = " + posicao);
+        }
+    }
+}
