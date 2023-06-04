33e52690a31cec27f2672ae875e070d9692f91b6
Renata Rodrigues <ticare1@hotmail.com>
2023-06-04 11:04:58 -0300

    Exercicios em C# - Mais Velho

---
+++ C#/mais_velho/mais_velho/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace mais_velho {
+    internal class Program {
+        static void Main(string[] args) {
+            int n, idadePessoaMaisVelha, posPessoaMaisVelha;
+
+            Console.Write("Quantas pessoas voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[n];
+            int[] idades = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Dados da " + (i + 1) + "a pessoa:");
+                Console.Write("Nome: ");
+                nomes[i] = Console.ReadLine();
+                Console.Write("Idade: ");
+                idades[i] = int.Parse(Console.ReadLine());
+            }
+
+            idadePessoaMaisVelha = idades[0];
+            posPessoaMaisVelha = 0;
+            for (int i = 0; i < n; i++) {
+                if (idades[i] > idadePessoaMaisVelha) {
+                    idadePessoaMaisVelha = idades[i];
+                    posPessoaMaisVelha = i;
+                }
+            }
+
+            Console.WriteLine("PESSOA MAIS VELHA: " + nomes[posPessoaMaisVelha]);
+        }
+    }
+}
