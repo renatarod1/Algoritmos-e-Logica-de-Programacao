0c6cd05c432dc803bfb0937116ba8bc000f84b7e
Renata Rodrigues <ticare1@hotmail.com>
2023-06-26 17:55:16 -0300

    Exercicios em C - Troco

---
+++ C/troco.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    double preco, vlRecebido, total, troco;
+    int qtd;
+
+    printf("Preco unitario do produto: ");
+    scanf("%lf", &preco);
+
+    printf("Quantidade comprada: ");
+    scanf("%d", &qtd);
+
+    printf("Dinheiro recebido: ");
+    scanf("%lf", &vlRecebido);
+
+    troco = vlRecebido - (preco * qtd);
+
+    printf("TROCO = %.2lf", troco);
+
+    return 0;
+}
