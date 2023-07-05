be10b4bcf2981e81e33367a68c757492730e34a1
Renata Rodrigues <ticare1@hotmail.com>
2023-07-05 17:46:46 -0300

    Exercicios em C - Multiplos

---
+++ C/multiplos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    printf("Digite dois numeros inteiros:\n");
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    if (num1 % num2 == 0 || num2 % num1 == 0){
+        printf("Sao multiplos");
+    } else {
+        printf("Nao sao multiplos");
+    }
+
+    return 0;
+}
