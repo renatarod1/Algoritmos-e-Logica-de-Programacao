d2814bac55441f4b3fac24655faa8710f9230fea
Renata Rodrigues <ticare1@hotmail.com>
2023-07-15 14:59:39 -0300

    Exercicios em C - Validação de Nota

---
+++ C/validacao_de_nota.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    double n1, n2, media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%lf", &n1);
+
+    while (n1 < 0 || n1 > 10.0){
+        printf("Valor invalido! Tente novamente: ");
+        scanf("%lf", &n1);
+    }
+
+    printf("Digite a segunda nota: ");
+    scanf("%lf", &n2);
+
+    while (n2 < 0 || n2 > 10.0){
+        printf("Valor invalido! Tente novamente: ");
+        scanf("%lf", &n2);
+    }
+
+    media = (n1 + n2) / 2;
+    printf("MEDIA =  %.2lf", media);
+
+    return 0;
+}
