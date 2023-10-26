606089e9cf7ec1060a6c2c337f68f09e23ec02a7
Renata Rodrigues <ticare1@hotmail.com>
2023-10-26 20:14:15 -0300

    Exercicios em Python - Menor dw Tres

---
+++ Python/menor_de_tres.py
@@ -0,0 +1,12 @@
+primeiro = int(input("Primeiro valor: "))
+segundo = int(input("Segundo valor: "))
+terceiro = int(input("Terceiro valor: "))
+
+if primeiro < segundo and primeiro < terceiro:
+    menor = primeiro
+elif segundo < terceiro:
+    menor = segundo
+else:
+    menor = terceiro
+
+print(f"MENOR = {menor}")
