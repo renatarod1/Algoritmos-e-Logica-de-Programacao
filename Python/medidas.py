d03ccb93c68c482b2175e7541432d33733227405
Renata Rodrigues <ticare1@hotmail.com>
2023-11-01 19:49:52 -0300

    Exercicios em Python - Medidas

---
+++ Python/medidas.py
@@ -0,0 +1,13 @@
+import math
+
+a = float(input("Digite a medida A: "))
+b = float(input("Digite a medida B: "))
+c = float(input("Digite a medida C: "))
+
+quadrado = math.pow(a, 2)
+triangulo = (a * b) / 2
+trapezio = ((a + b) * c) / 2
+
+print(f"AREA DO QUADRADO = {quadrado:.4f}")
+print(f"AREA DO TRIANGULO = {triangulo:.4f}")
+print(f"AREA DO TRAPEZIO = {trapezio:.4f}")
