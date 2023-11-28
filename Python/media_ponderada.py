b8602a5ae1d876d608d7c0970969f7bfdb248beb
Renata Rodrigues <ticare1@hotmail.com>
2023-11-28 18:35:56 -0300

    Exercicios em Python - Media Ponderada

---
+++ Python/media_ponderada.py
@@ -0,0 +1,9 @@
+n =  int(input("Quantos casos voce vai digitar? "))
+
+for i in range(0, 3):
+    print("Digite tres numeros:")
+    n1 = float(input())
+    n2 = float(input())
+    n3 = float(input())
+    media = (n1 * 2 + n2 * 3 + n3 * 5) / 10.0
+    print(f"MEDIA = {media:.1f}")
